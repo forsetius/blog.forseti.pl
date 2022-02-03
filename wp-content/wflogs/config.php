ba3393aa70cfc8cd5fdb498e6a65c04c1b1b047e
Marcin Paździora <forseti.pl@gmail.com>
2022-02-03 19:41:10 +0100

    Maintenance backup

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1642544326;s:7:"authKey";s:64:"0S?[Nm58EF!Xk Tl?+-yDFSQWicEK`{i@LW=iz[AP[^ Sb$r8Bk~}sG2!/V#uT4b";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1965;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"0S?[Nm58EF!Xk Tl?+-yDFSQWicEK`{i@LW=iz[AP[^ Sb$r8Bk~}sG2!/V#uT4b";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1965;}
